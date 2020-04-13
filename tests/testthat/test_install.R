73e483675e28f9e63d286e9854bd036be4d69b1f
Andreas Blaette <andreas.blaette@uni-due.de>
2020-04-13 13:17:01 +0200

    tarball test reworked

--- tests/testthat/test_install.R
+++ tests/testthat/test_install.R
@@ -6,8 +6,8 @@ test_that(
     library(polmineR)
     file.remove(list.files(registry(), full.names = TRUE))
     registry_reset(registryDir = registry())
-    
-    tmp_tarball <- tempfile(pattern = ".tar.gz")
+
+    tmp_tarball <- tempfile(fileext = ".tar.gz")
     polmineR_registry <- system.file(package = "polmineR", "extdata", "cwb", "registry")
     cwbtools::corpus_as_tarball(
       corpus = "REUTERS",
@@ -17,10 +17,16 @@ test_that(
     )
     
     prefix <- file.path(tempdir(), "cwb")
-    if (!dir.exists(prefix)) dir.create(prefix)
+    if (dir.exists(prefix)) unlink(prefix, recursive = TRUE)
+    dir.create(prefix)
+    
     cwb_dirs <- create_cwb_directories(prefix = prefix, ask = FALSE)
     
-    corpus_install(tarball = tmp_tarball, registry_dir = cwb_dirs[["registry_dir"]], corpus_dir = cwb_dirs[["registry_dir"]])
+    corpus_install(
+      tarball = tmp_tarball, 
+      registry_dir = cwb_dirs[["registry_dir"]], 
+      corpus_dir = cwb_dirs[["corpus_dir"]]
+    )
     
     file.copy(
       from = file.path(cwb_dirs[["registry_dir"]], "reuters"),
@@ -34,34 +40,6 @@ test_that(
 
 
 test_that(
-  "",
-  {
-    doi <- "https://doi.org/10.5281/zenodo.3748858"
-    record_id <- gsub("^.*?10\\.5281/zenodo\\.(\\d+)$", "\\1", doi)
-    zenodo_api_url <- sprintf("https://zenodo.org/api/records/%d", as.integer(record_id))
-    zenodo_data <- jsonlite::fromJSON(RCurl::getURL(zenodo_api_url))
-    tarball <- grep("\\.tar\\.gz$", zenodo_data[["files"]][["links"]][["self"]], value = TRUE)
-    
-    cwbtools_tmpdir <- file.path(normalizePath(tempdir(), winslash = "/"), "cwbtools_tmpdir", fsep = "/")
-    dir.create(cwbtools_tmpdir)
-    corpus_tarball <- file.path(cwbtools_tmpdir, basename(tarball), fsep = "/")
-    curl::curl_download(url = tarball, destfile = corpus_tarball, quiet = FALSE)
-    untar(tarfile = corpus_tarball, exdir = cwbtools_tmpdir)
-    tmp_registry_dir <- file.path(normalizePath(cwbtools_tmpdir, winslash = "/"), "registry", fsep = "/")
-    tmp_data_dir <- file.path(normalizePath(cwbtools_tmpdir, winslash = "/"), "indexed_corpora", fsep = "/")
-    home_dir <- file.path(tmp_data_dir, tolower("ungamini"), fsep = "/")
-    files <- list.files(home_dir)
-    
-    expect_identical(length(files), 64L)
-    registry_data <- registry_file_parse(corpus = "ungamini", registry_dir = tmp_registry_dir)
-    registry_data[["home"]] <- home_dir
-    registry_file_write(data = registry_data, corpus = "ungamini", registry_dir = tmp_registry_dir)
-    data_files_to_copy <- list.files(home_dir, full.names = TRUE)
-    expect_identical(length(data_files_to_copy), 64L)
-  }
-)
-
-test_that(
   "download and install UNGAMINI",
   {
     skip_on_cran()
@@ -71,9 +49,14 @@ test_that(
     Sys.setenv(CORPUS_REGISTRY = cwb_dirs[["registry_dir"]])
     
     corpus_install(doi = "https://doi.org/10.5281/zenodo.3748858", registry_dir = cwb_dirs[["registry_dir"]], corpus_dir = cwb_dirs[["corpus_dir"]])
-    # library(polmineR)
-    # options("")
-    # registry_reset(registryDir = registry())
-    # expect_true("UNGAMINI" %in% corpus()[["corpus"]])
+    
+    library(polmineR)
+    file.copy(
+      from = file.path(cwb_dirs[["registry_dir"]], "ungamini"),
+      to = file.path(registry(), "ungamini")
+    )
+    registry_reset(registryDir = registry())
+    
+    expect_true("UNGAMINI" %in% corpus()[["corpus"]])
   }
 )
