9027c5659acf9d9ffcbbe97dbad72b6b3c5d70f5
Andreas Blaette <andreas.blaette@uni-due.de>
2020-04-13 08:58:03 +0200

    breaking down tests

--- tests/testthat/test_directories.R
+++ tests/testthat/test_directories.R
@@ -1,19 +1,31 @@
 testthat::context("create CWB directories")
 
 test_that(
-  "test function cwb_registry_dir()",
+  "cwb_registry_dir() - NULL if CORPUS_REGISTRY unset",
   {
     Sys.setenv(CORPUS_REGISTRY = "")
     expect_null(cwb_registry_dir())
-    
+  }
+)
+
+
+test_that(
+  "cwb_registry_dir() - use polmineR dir",
+  {
     polmineR_registry <- system.file(package = "polmineR", "extdata", "cwb", "registry")
     Sys.setenv(CORPUS_REGISTRY = polmineR_registry)
     expect_identical(polmineR_registry, cwb_registry_dir())
-    
+  }
+)
+
+test_that(
+  "cwb_registry_dir()",
+  {
+    polmineR_registry <- system.file(package = "polmineR", "extdata", "cwb", "registry")
     library(polmineR)
     expect_identical(polmineR_registry, cwb_registry_dir())
   }
-)
+)    
 
 test_that(
   "test function cwb_corpus_dir()",
