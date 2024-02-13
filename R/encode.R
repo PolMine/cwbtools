279a6123df755aaf0cacf5deba3589d0ad19eb71
Andreas Blätte <andreasblatte@MBP-von-Andreas.fritz.box>
2024-02-13 16:58:13 +0100

    initial draft of encode method

---
+++ R/encode.R
@@ -0,0 +1,233 @@
+#' Encode CWB Corpus.
+#' 
+#' @param x A `data.frame` or an object inheriting from `data.frame` (such as 
+#'   `tibble`, `data.table`).
+#' @param s_attributes A `list` of `data.frame` objects with columns 'cpos_left'
+#'   and 'cpos_right' and columns with s-attributes, the names of which will 
+#'   serve as names of s-attributes. It `s_attributes` is a `data.frame`, it will
+#'   be coerced to a `list`.
+#' @param reload A logical value, whether to reload the corpus to make it
+#'   immediatedly available.
+#' @param ... Further arguments (unused).
+#' @inheritParams p_attribute_encode
+#' @rdname encode
+#' @exportMethod encode
+setGeneric("encode", function(x, ...) standardGeneric("encode"))
+
+#' @examples
+#' library(dplyr)
+#' library(tidytext)
+#' 
+#' registry_tmp <- fs::path(tempdir(), "cwb_registry")
+#' dir.create(registry_tmp)
+#' 
+#' tokenstream <- quanteda::data_char_ukimmig2010 %>%
+#'    as.data.frame() %>%
+#'    as_tibble(rownames = "party") %>%
+#'    rename(`text` = ".") %>%
+#'    unnest_tokens(word, text, to_lower = FALSE, strip_punct = FALSE) %>%
+#'    mutate(cpos = 0L:(nrow(.) - 1L))
+#'    
+#' metadata <- tokenstream %>% 
+#'   group_by(party) %>% 
+#'   summarise(cpos_left = min(cpos), cpos_right = max(cpos))
+#' 
+#' tokenstream %>%
+#'   select(-cpos) %>%
+#'   encode(corpus = "UKIMMIG2010", s_attributes = metadata)
+#' @rdname encode
+setMethod("encode", "data.frame", function(
+    x,
+    corpus,
+    s_attributes = NULL,
+    encoding = "utf8",
+    registry_dir = fs::path(tempdir(), "cwb_registry"),
+    data_dir = fs::path(tempdir(), "cwb_data_dir", tolower(corpus)),
+    method = c("R", "CWB"),
+    verbose = TRUE,
+    compress = FALSE,
+    reload = TRUE,
+    quietly = TRUE
+){
+  if (!"word" %in% colnames(x)){
+    cli_alert_danger("Column 'word' required but missing.")
+    return(FALSE)
+  }
+  # add check on validity of p-attributes to be encoded
+  
+  if (inherits(s_attributes, "data.frame")) s_attributes <- list(s_attributes)
+  for (i in seq_along(s_attributes)){
+    if (!inherits(s_attributes[[i]], "data.frame")){
+      cli_alert_danger("s_attribute is not a data.frame or an object inheriting from data.frame")
+      return(FALSE)
+    }
+    if (!all(c("cpos_left", "cpos_right") %in% colnames(s_attributes[[i]]))){
+      cli_alert_danger("columns 'cpos_left' and 'cpos_right' missing")
+      return(FALSE)
+    }
+  }
+  s_attr_names <- setdiff(
+    unlist(lapply(s_attributes, colnames)),
+    c("cpos_left", "cpos_right")
+  )
+  # add check on validity of s-attributes to be encoded
+
+  if (verbose) cli_rule("Prepare encoding corpus {corpus}")
+  
+  # check registry file and registry directory ---------------------------------
+  if (file.exists(registry_dir))
+    if (file.info(registry_dir)[["isdir"]] != TRUE)
+      stop("registry_dir is not a directory")
+  if (verbose)
+    cli_alert_info("registry directory: {.path {registry_dir}}")
+  
+  registry_file <- fs::path(registry_dir, tolower(corpus))
+  
+  if (file.exists(registry_file)){
+    cli_alert_warning(
+      "registry file for corpus {.val {corpus}} already exists"
+    )
+    corpus_remove(corpus = corpus, registry_dir = registry_dir)
+  }
+  
+  if (!file.exists(data_dir)) dir.create(data_dir, recursive = TRUE)
+  
+  if (verbose) cli_alert_info("data directory: {.path {data_dir}}")
+  
+  # checking whether the encoding is ok should be part of p_attribute_encode()
+  # and s_attribute_encode()
+  if (!encoding %in% c("ascii", paste("latin", 1:9, sep = ""), "utf8")){
+    stop(
+      "encoding is required to be either ascii, latin1 to latin9, or utf8"
+    )
+  }
+  if (verbose) cli_alert_info("encoding: {.val {encoding}}")
+  
+  if (length(method) > 1) method <- method[1]
+  if (verbose)
+    cli_alert_info(
+      "method for encoding, indexing and compression: {.val {method}}"
+    )
+  
+  if (verbose) cli_rule("encode p-attribute {.val word}")
+  p_attribute_encode(
+    token_stream = x[["word"]],
+    corpus = corpus,
+    encoding = encoding,
+    registry_dir = registry_dir,
+    data_dir = data_dir,
+    method = method,
+    verbose = verbose,
+    compress = compress,
+    quietly = quietly
+  )
+  
+  # add other p-attributes than 'word'
+  if (ncol(x) > 1L){
+    for (p_attr in setdiff(colnames(x), "word")){
+      if (verbose) cli_rule("encode p-attribute {.val {p_attr}}")
+      p_attribute_encode(
+        token_stream = x[[p_attr]],
+        corpus = corpus, 
+        p_attribute = p_attr,
+        encoding = encoding,
+        registry_dir = registry_dir,
+        data_dir = data_dir,
+        method = method,
+        verbose = verbose,
+        compress = compress,
+        quietly = quietly
+      )
+    }
+  }
+  
+  if (verbose) cli_rule("Encode s-attributes")
+  for (i in seq_along(s_attributes)){
+    s_attrs <- setdiff(
+      colnames(s_attributes[[i]]),
+      c("cpos_left", "cpos_right"))
+    for (s_attr in s_attrs){
+      if (verbose) cli_alert_info("encode s-attribute {.val {s_attr}}")
+      s_attribute_encode(
+        values = s_attributes[[i]][[s_attr]],
+        corpus = corpus,
+        s_attribute = s_attr,
+        region_matrix = as.matrix(
+          s_attributes[[i]][,c("cpos_left", "cpos_right")]
+        ),
+        data_dir = data_dir,
+        registry_dir = registry_dir,
+        encoding = encoding,
+        method = method,
+        verbose = verbose
+      )
+    }
+  }
+  
+  if (verbose) cli_rule("Prepare registry file")
+  if (verbose) cli_progress_step("write registry file")
+  reg_data <- registry_data(
+    name = toupper(corpus),
+    id = tolower(corpus),
+    home = path.expand(data_dir),
+    properties = c(charset = encoding), 
+    p_attributes = colnames(x),
+    s_attributes = s_attr_names
+  )
+  registry_file_write(
+    data = reg_data,
+    corpus = tolower(corpus),
+    registry_dir = registry_dir
+  )
+  if (verbose) cli_progress_done()
+  
+  if (verbose) cli_rule("Check result")
+  if (isTRUE(reload)){
+    
+    cl_delete_corpus(corpus = corpus, registry = registry_dir)
+    if (is.null(cl_find_corpus(corpus = corpus, registry = registry_dir))){
+      if (verbose) cli_alert_success("corpus unloaded")
+    } else {
+      if (verbose) cli_alert_danger("unloading corpus failed")
+    }
+    
+    # Not too intuitive, but this (re)loads the corpus
+    size <- cl_attribute_size(
+      corpus = corpus,
+      attribute = "word",
+      attribute_type = "p",
+      registry = registry_dir
+    )
+    
+    if (typeof(cl_find_corpus(corpus, registry = registry_dir)) == "externalptr"){
+      if (verbose)
+        cli_alert_success("corpus reloaded (size: {.val {size}})")
+    } else {
+      if (verbose) cli_alert_danger("reloading corpus failed (CL representation")
+    }
+    
+    if (!cqp_load_corpus(corpus = corpus, registry = registry_dir)){
+      if (verbose)
+        cli_alert_danger(
+          "reloading corpus failed (CQP representation not available)"
+        )
+    }
+  }
+  
+  p_attrs <- corpus_p_attributes(corpus = corpus, registry = registry_dir)
+  if (all(colnames(x) %in% p_attrs)){
+    if (verbose) cli_alert_success("all p-attributes are available")
+  } else {
+    cli_alert_danger("not all p-attributes available")
+  }
+  
+  s_attrs <- corpus_s_attributes(corpus = corpus, registry = registry_dir)
+  if (all(s_attr_names %in% s_attrs)){
+    if (verbose) cli_alert_success("all s-attributes are available")
+  } else {
+    cli_alert_danger("not all s-attributes available")
+  }
+  
+  invisible(TRUE)
+})
+
